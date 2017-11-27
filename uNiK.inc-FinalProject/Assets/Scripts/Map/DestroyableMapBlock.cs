f911566b9eb001b462e1331ea75397cbf343ce00
Nathan Chica <n_chica@u.pacific.edu>
2017-11-27 03:05:40 -0800

    Blocks are now destroyable by projectiles and explosions, see in ShootingSystem scene

---
+++ uNiK.inc-FinalProject/Assets/Scripts/Map/DestroyableMapBlock.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyableMapBlock : MonoBehaviour {
+
+    [SerializeField] private GameObject particleEffects;
+
+    private GameObject spawnedParticleEffects;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if (collision.CompareTag("Attack")) {
+        Debug.Log("here");
+            this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            this.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            spawnedParticleEffects = GameObject.Instantiate(particleEffects, 
+                this.gameObject.GetComponent<Transform>());
+            ParticleSystem particleSys = spawnedParticleEffects.GetComponent<ParticleSystem>();
+            var particleSysMain = particleSys.main;
+            particleSysMain.startColor = this.gameObject.GetComponent<SpriteRenderer>().color;
+            Destroy(spawnedParticleEffects, particleSysMain.duration);
+            Destroy(this.gameObject, particleSysMain.duration);
+        //}
+    }
+}
