313e681d7a1d80668a7a9c1b4baee25037b5f10f
Nathan Chica <n_chica@u.pacific.edu>
2017-11-27 15:23:24 -0800

    Added a exploding bouncy ball attack in ShootingSystem scene, added relevant scripts and images

---
+++ uNiK.inc-FinalProject/Assets/Scripts/Attacks/ExplodingBouncyBall.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodingBouncyBall : MonoBehaviour {
+
+    [SerializeField] private float m_ExplosionTimer;
+    [SerializeField] private GameObject m_ExplosionObject;
+
+    private Animator m_Animator;
+    private Rigidbody2D m_Rigidbody2D;
+    private GameObject m_SpawnedExplosionObj;
+
+	// Use this for initialization
+	void Start () {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+
+        Invoke("Explode", m_ExplosionTimer);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        m_Animator.speed = Mathf.Clamp01(m_Rigidbody2D.velocity.magnitude);
+	}
+
+    private void Explode()
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        m_SpawnedExplosionObj = GameObject.Instantiate(m_ExplosionObject, transform.position, Quaternion.identity);
+        ParticleSystem explosion = m_ExplosionObject.GetComponent<ParticleSystem>();
+
+        Destroy(m_SpawnedExplosionObj, explosion.main.duration);
+        Destroy(this.gameObject, explosion.main.duration);
+    }
+}
--- uNiK.inc-FinalProject/Assets/Scripts/Map/DestroyableMapBlock.cs
+++ uNiK.inc-FinalProject/Assets/Scripts/Map/DestroyableMapBlock.cs
@@ -20,16 +20,21 @@ public class DestroyableMapBlock : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //if (collision.CompareTag("Attack")) {
-            this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
-            this.gameObject.GetComponent<BoxCollider2D>().enabled = false;
-            spawnedParticleEffects = GameObject.Instantiate(particleEffects, 
-                this.gameObject.GetComponent<Transform>());
-            ParticleSystem particleSys = spawnedParticleEffects.GetComponent<ParticleSystem>();
-            var particleSysMain = particleSys.main;
-            particleSysMain.startColor = this.gameObject.GetComponent<SpriteRenderer>().color;
-            Destroy(spawnedParticleEffects, particleSysMain.duration);
-            Destroy(this.gameObject, particleSysMain.duration);
-        //}
+        if (!collision.CompareTag("Player")) {
+            DestroyBlock();
+        }
+    }
+
+    public void DestroyBlock()
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        this.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        spawnedParticleEffects = GameObject.Instantiate(particleEffects,
+            this.gameObject.GetComponent<Transform>());
+        ParticleSystem particleSys = spawnedParticleEffects.GetComponent<ParticleSystem>();
+        var particleSysMain = particleSys.main;
+        particleSysMain.startColor = this.gameObject.GetComponent<SpriteRenderer>().color;
+        Destroy(spawnedParticleEffects, particleSysMain.duration);
+        Destroy(this.gameObject, particleSysMain.duration);
     }
 }
