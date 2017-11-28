f5ddd62af6d50c8d3afdc583494eb3f6daa8aa93
Nathan Chica <n_chica@u.pacific.edu>
2017-11-28 00:44:35 -0800

    New Projectile: Super Bouncy Ball, high bounciness, destroys destroyable blocks on bounce

---
+++ uNiK.inc-FinalProject/Assets/Scripts/Attacks/SuperBouncyBall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperBouncyBall : MonoBehaviour {
+
+    [SerializeField] private float m_Damage;
+    [SerializeField] private float m_Duration;
+
+    private void Awake()
+    {
+        Destroy(this.gameObject, m_Duration);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("DestroyableBlock"))
+        {
+            collision.gameObject.GetComponent<DestroyableMapBlock>().DestroyBlock();
+        }
+    }
+}
