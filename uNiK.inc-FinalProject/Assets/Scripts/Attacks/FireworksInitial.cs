f2ec0734759d276a66f1f804684b582fc0cebe2e
Nathan Chica <n_chica@u.pacific.edu>
2017-11-29 01:04:26 -0800

    adding some particle effect assets

--- uNiK.inc-FinalProject/Assets/Scripts/Attacks/FireworksInitial.cs
+++ uNiK.inc-FinalProject/Assets/Scripts/Attacks/FireworksInitial.cs
@@ -6,6 +6,7 @@ public class FireworksInitial : MonoBehaviour {
 
     [SerializeField] private GameObject fireworksObj;
     [SerializeField] private float duration;
+    [SerializeField] private bool isSprite;
 
     private void Awake()
     {
@@ -14,10 +15,20 @@ public class FireworksInitial : MonoBehaviour {
 
     private void SpawnFireworks()
     {
-        this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
-        this.gameObject.GetComponent<CircleCollider2D>().enabled = false;
-        this.gameObject.GetComponentInChildren<ParticleSystem>().Clear();
-        this.gameObject.GetComponentInChildren<ParticleSystem>().Stop();
+        if (isSprite)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            this.gameObject.GetComponent<CircleCollider2D>().enabled = false;
+            this.gameObject.GetComponentInChildren<ParticleSystem>().Clear();
+            this.gameObject.GetComponentInChildren<ParticleSystem>().Stop();
+        }
+        else
+        {
+            this.gameObject.GetComponent<ParticleSystem>().Clear();
+            this.gameObject.GetComponent<ParticleSystem>().Stop();
+            this.gameObject.GetComponent<CircleCollider2D>().enabled = false;
+        }
+        
         GameObject.Instantiate(fireworksObj, transform.position, Quaternion.identity);
 
         Destroy(this.gameObject, 2.0f);
