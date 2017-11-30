6102fcc18521bf6d71ce2e5e1582704f80c86b5c
Spencer McKean <s_mckean@u.pacific.edu>
2017-11-30 00:27:20 -0800

    working on item spawner

---
+++ uNiK.inc-FinalProject/Assets/Scripts/Map/ItemSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour {
+
+    private Rigidbody2D[] itemsList;
+
+    // Use this for initialization
+    void Start () {
+        itemsList = Resources.LoadAll<Rigidbody2D>("Items");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void spawnItem()
+    {
+        
+    }
+}
