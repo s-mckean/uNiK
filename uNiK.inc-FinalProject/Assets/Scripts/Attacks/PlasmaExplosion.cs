6704c91e8a4160cedef856e02be90b65f4543496
Nathan Chica <n_chica@u.pacific.edu>
2017-12-08 12:34:06 -0800

    adding files

---
+++ uNiK.inc-FinalProject/Assets/Scripts/Attacks/PlasmaExplosion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlasmaExplosion : MonoBehaviour {
+
+    [SerializeField] private GameObject firePrefab;
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Instantiate(firePrefab, other.transform.position, Quaternion.identity);
+    }
+
+}
