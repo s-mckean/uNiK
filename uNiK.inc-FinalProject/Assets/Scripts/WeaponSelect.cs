c08b936f3258f5895ee688e43b49ddbd8a2f5d7f
Spencer McKean <s_mckean@u.pacific.edu>
2017-12-08 12:12:00 -0800

    Weapon buttons are now disabled when the menu is opened

--- uNiK.inc-FinalProject/Assets/Scripts/WeaponSelect.cs
+++ uNiK.inc-FinalProject/Assets/Scripts/WeaponSelect.cs
@@ -43,6 +43,7 @@ public class WeaponSelect : MonoBehaviour
         if (Input.GetKeyDown("e") && interactableMenu)
         {
             OpenWeaponMenu();
+            DisableWeaponButtons();
         }
     }
 
