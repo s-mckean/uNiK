07e4f3adbc464fe40ebdd86ded631d1f42b1dd3c
Kevin Huynh <k_huynh13@u.pacific.edu>
2017-12-07 23:17:54 -0800

    Modified the explosion for the rocket so the visuals fit the actual hitboxes and added another tag PassThrough when put on projectiles, they don't destroy the ground

--- uNiK.inc-FinalProject/Assets/Scripts/Map/DestroyableMapBlock.cs
+++ uNiK.inc-FinalProject/Assets/Scripts/Map/DestroyableMapBlock.cs
@@ -20,7 +20,7 @@ public class DestroyableMapBlock : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!collision.CompareTag("Player")) {
+        if (!collision.CompareTag("Player") && !collision.CompareTag("PassThrough")) {
             DestroyBlock();
         }
     }
